1f64fec03178cb329b1c4c1567ba4d1dd91e1a4b
Michael Iodice <miodice3@gmail.com>
2021-01-21 14:49:30 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+// function divide(num1, num2){
+//     console.log("num1 = ", num1, "num2: ", num2, "answer = ", (num1/num2))
+//     return (num1 / num2);
+// }
+
+function divide(){
+    return (2000/100)
+}
+
+let square = x => x**2;
+
+let add = (x, y) => x + y;
